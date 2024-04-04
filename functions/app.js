7faa0b6d03e8d53443c17fd3219e4ffd1530443b
Daniel De Torres <88454993+dangrishh@users.noreply.github.com>
2024-04-04 13:50:19 +0800

    deploying on webserver

--- functions/app.js
+++ functions/app.js
@@ -36,6 +36,6 @@ module.exports.handler = serverless(app);
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is Scuccessfully running on http://localhost:${PORT}`);
 });
 
