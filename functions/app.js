078be4cf633b64bbb6c4b0b529dea6aef7c9912a
Daniel De Torres <88454993+dangrishh@users.noreply.github.com>
2024-04-04 13:39:42 +0800

    netlify init

--- functions/app.js
+++ functions/app.js
@@ -2,6 +2,7 @@ const express =  require('express');
 const path    =  require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const serverless = require("serverless-http");
 require('dotenv').config();
 
 
@@ -29,8 +30,12 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(userRouter);
 app.use("/admin" ,adminRouter);
 
+app.use("/.netlify/functions/app", router);
+module.exports.handler = serverless(app);
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
